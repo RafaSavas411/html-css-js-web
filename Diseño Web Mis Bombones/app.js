7ef8f18be1de786f179f0c6102f11897e01f70fe
RafaSavas411 <112649886+RafaSavas411@users.noreply.github.com>
2022-09-13 21:46:43 -0300

    Add files via upload

--- Diseño Web Mis Bombones/app.js
+++ Diseño Web Mis Bombones/app.js
@@ -53,18 +53,4 @@ const hideMobileMenu = () => {
 };
 
 menuLinks.addEventListener('click', hideMobileMenu);
-navLogo.addEventListener('click', hideMobileMenu);
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+navLogo.addEventListener('click', hideMobileMenu)
